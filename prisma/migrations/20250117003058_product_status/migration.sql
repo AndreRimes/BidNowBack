11345ad19a0de69b0a26cf0d808c3c229b1c5257
AndreRimes <andre.2004.rimes@gmail.com>
2025-01-17 17:55:45 -0300

    a

--- prisma/migrations/20250117003058_product_status/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Status" AS ENUM ('ACTIVE', 'SOLD');
-
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "status" "Status" NOT NULL DEFAULT 'ACTIVE';
--- prisma/migrations/20250117205203_tudo/migration.sql
+++ prisma/migrations/20250117205203_tudo/migration.sql
@@ -1,3 +1,18 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('ACTIVE', 'SOLD');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateTable
 CREATE TABLE "Product" (
     "id" TEXT NOT NULL,
@@ -6,6 +21,7 @@ CREATE TABLE "Product" (
     "minimalPrice" DOUBLE PRECISION NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'ACTIVE',
     "userId" TEXT NOT NULL,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
@@ -33,6 +49,9 @@ CREATE TABLE "File" (
 
     CONSTRAINT "File_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Product" ADD CONSTRAINT "Product_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
